e8c00d4edeb7bec118eea8592920f69bcca8fe28
Cilyan Olowen <gaknar@gmail.com>
2015-01-11 16:39:34 +0100

    Move to autotools
    Although this is a very simple and light utility, using autotools allows
    to easily cross-compile and make the tool more reacheable to other
    users.
    
    Signed-off-by: Cilyan Olowen <gaknar@gmail.com>

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install it ***"
+        exit 1
+fi
+
+autoreconf --force --install --verbose || exit $?
